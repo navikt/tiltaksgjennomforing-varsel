17e2701ea158282e7759a766da9d23df89a1fc03
Sindre Dahl Løken <sindre.loken@nav.no>
2023-12-18 13:27:42 +0100

    forsøk på å vise hva som feiler (#102)
    * forsøk på å vise hva som feiler
    
    * handtering av feilkoder fra altinn
    
    * fikset opp i ting

--- src/main/java/no/nav/tag/tiltaksgjennomforing/varsel/altinnvarsel/AltinnVarselAdapter.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/varsel/altinnvarsel/AltinnVarselAdapter.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import no.altinn.schemas.serviceengine.formsengine._2009._10.TransportType;
 import no.altinn.schemas.services.serviceengine.notification._2009._10.*;
 import no.altinn.schemas.services.serviceengine.standalonenotificationbe._2009._10.StandaloneNotificationBEList;
+import no.altinn.services.common.fault._2009._10.AltinnFault;
 import no.altinn.services.serviceengine.notification._2010._10.INotificationAgencyExternalBasic;
 import no.altinn.services.serviceengine.notification._2010._10.INotificationAgencyExternalBasicSendStandaloneNotificationBasicV3AltinnFaultFaultFaultMessage;
 import no.nav.tag.tiltaksgjennomforing.exceptions.AltinnException;
@@ -12,6 +13,8 @@ import org.springframework.stereotype.Service;
 
 import javax.xml.bind.JAXBElement;
 import javax.xml.namespace.QName;
+
+import static no.nav.tag.tiltaksgjennomforing.varsel.altinnvarsel.AltinnFunksjonellFeil.erFunksjonellFeil;
 
 @Service
 @Slf4j
@@ -53,9 +56,49 @@ public class AltinnVarselAdapter {
                     varselProperties.getSystemBruker(),
                     varselProperties.getSystemPassord(),
                     standaloneNotification);
-        } catch (INotificationAgencyExternalBasicSendStandaloneNotificationBasicV3AltinnFaultFaultFaultMessage | RuntimeException e) {
+        } catch (INotificationAgencyExternalBasicSendStandaloneNotificationBasicV3AltinnFaultFaultFaultMessage e) {
+            log.error("Feil ved varsling gjennom Altinn", e);
+            prosesserVarselFeil(e);
+            throw new AltinnException("Feil ved varsling gjennom Altinn");
+        } catch (RuntimeException e) {
             log.error("Feil ved varsling gjennom Altinn", e);
             throw new AltinnException("Feil ved varsling gjennom Altinn");
         }
     }
+
+    private void prosesserVarselFeil(INotificationAgencyExternalBasicSendStandaloneNotificationBasicV3AltinnFaultFaultFaultMessage e) {
+        final String altinnErrorMessage = constructAltinnErrorMessage(e);
+        Integer feilkode = getFeilkode(e);
+        log.error("Feil ved varsling gjennom Altinn. Feilkode: {}", feilkode);
+        if (erFunksjonellFeil(feilkode)) {
+            log.error("Funksjonell feil i kall mot Altinn. ErorrMessage: {}", altinnErrorMessage);
+        }
+    }
+
+    // Liste over errorID: https://altinn.github.io/docs/api/tjenesteeiere/soap/grensesnitt/varseltjeneste/#feilsituasjoner
+    private String constructAltinnErrorMessage(INotificationAgencyExternalBasicSendStandaloneNotificationBasicV3AltinnFaultFaultFaultMessage e) {
+        AltinnFault faultInfo = e.getFaultInfo();
+        if (faultInfo == null) {
+            return e.getMessage();
+        }
+        return "errorGuid=" + unwrap(faultInfo.getErrorGuid()) + ", " +
+                "userGuid=" + unwrap(faultInfo.getUserGuid()) + ", " +
+                "errorId=" + faultInfo.getErrorID() + ", " +
+                "errorMessage=" + unwrap(faultInfo.getAltinnErrorMessage());
+    }
+    private Integer getFeilkode(INotificationAgencyExternalBasicSendStandaloneNotificationBasicV3AltinnFaultFaultFaultMessage e) {
+        AltinnFault faultInfo = e.getFaultInfo();
+        if (faultInfo == null) {
+            return null;
+        }
+        return faultInfo.getErrorID();
+    }
+    private String unwrap(JAXBElement<String> jaxbElement) {
+        if (jaxbElement == null) {
+            return "null";
+        }
+        return jaxbElement.getValue();
+    }
+
+
 }
